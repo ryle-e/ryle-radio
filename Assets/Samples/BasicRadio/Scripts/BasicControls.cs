fd1653a73757b5223d09624f67e68de3b69ef560
ryley/home <33923649+ryle-e@users.noreply.github.com>
2025-10-28 01:18:42 +1100

    fixed samples not using the right model

---
+++ Assets/Samples/BasicRadio/Scripts/BasicControls.cs
@@ -0,0 +1,39 @@
+using RyleRadio.Components;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicControls : MonoBehaviour
+{
+    [SerializeField] private RadioOutput output;
+    [SerializeField] private Text text;
+    [SerializeField] private RectTransform indicator;
+
+    [Space(8)]
+    [SerializeField] private Vector2 indicatorXRange;
+
+    [Space(8)]
+    [SerializeField] private float tuneSpeed = 200;
+
+    private void Update()
+    {
+        // if shift is held down, tune faster
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            tuneSpeed *= 2;
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            tuneSpeed /= 2;
+
+        // move the tune left and right based on keys
+        if (Input.GetKey(KeyCode.Q))
+            output.Tune -= tuneSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            output.Tune += tuneSpeed * Time.deltaTime;
+
+        // set the text showing the tune number
+        text.text = (Mathf.Round(output.DisplayTune * 10) / 10).ToString();
+
+        // move the indicator to the correct position
+        indicator.transform.localPosition = new Vector3(Mathf.Lerp(indicatorXRange.x, indicatorXRange.y, output.Tune01), 0, 0);
+    }
+}
---
+++ Assets/Samples/InteractionComponents/Scripts/InteractorEventFiller.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractorEventFiller : MonoBehaviour
+{
+    [System.Serializable]
+    private class FillerEvent
+    {
+        public KeyCode key;
+        public UnityEvent uEvent;
+    }
+
+    [SerializeField] private List<FillerEvent> events;
+
+    private void Update()
+    {
+        foreach (var e in events)
+            if (Input.GetKeyDown(e.key))
+                e.uEvent.Invoke();
+    }
+}
