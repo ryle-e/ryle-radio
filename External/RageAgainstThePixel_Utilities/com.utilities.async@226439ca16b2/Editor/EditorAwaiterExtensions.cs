b8facee6c376fb9bbe8d11194af38daaf9a3085c
ryley/home <33923649+ryle-e@users.noreply.github.com>
2025-11-26 21:27:26 +1100

    trying another dependency cleanup

--- External/RageAgainstThePixel_Utilities/com.utilities.async@226439ca16b2/Editor/EditorAwaiterExtensions.cs
+++ External/RageAgainstThePixel_Utilities/com.utilities.async@226439ca16b2/Editor/EditorAwaiterExtensions.cs
@@ -1,12 +1,14 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Unity.EditorCoroutines.Editor;
+//using Unity.EditorCoroutines.Editor;
 
 namespace Utilities.Async.Editor
 {
+    /*
     public static class EditorAwaiterExtensions
     {
         public static YieldInstructionAwaiter GetAwaiter(this EditorWaitForSeconds instruction)
             => new(instruction);
     }
+    */
 }
